a04740f0d38e18cf653a7da95d4bff217195ce1c
rafi <rafiderberliner@googlemail.com>
2022-10-10 11:44:30 +0200

    Create dir if it does not exist

--- scripts/create_debug_split.py
+++ scripts/create_debug_split.py
@@ -1,7 +1,7 @@
 from scripts.read_trajectories import read_compressed_json_file,\
     filter_preprocessed_data,\
     save_compressed_json_file
-
+import os
 """
 Script can be used to create smaller split files for debugging purpose.
 Create this directory first:
@@ -9,11 +9,14 @@ data/datasets/R2R_VLNCE_v1-3_preprocessed/debug
 """
 if __name__ == "__main__":
     file1 = "../data/datasets/R2R_VLNCE_v1-3_preprocessed/train/train.json.gz"
-    outpath = "../data/datasets/R2R_VLNCE_v1-3_preprocessed/debug/debug.json.gz"
+    outpath = "../data/datasets/R2R_VLNCE_v1-3_preprocessed/debug"
+
+    if not os.path.exists(outpath):
+        os.makedirs(outpath)
     train_file = read_compressed_json_file(file1)
     # That was a problematic episode when creating a video for the gold path
     episode_filter = [118]
     print("Filtering:", episode_filter)
     filter_preprocessed_data(train_file, episode_filter)
-    save_compressed_json_file(train_file, outpath)
+    save_compressed_json_file(train_file, outpath+"/debug.json.gz")
     print("Done!")
